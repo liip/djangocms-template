dff0522d3a8c52b2ad8e840f2a2df5c4e6f95c81
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-07-21 14:37:31 +0200

    add the possibility to use Kanbasu

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -53,16 +53,55 @@ def patch_playbook(path):
         f.write(''.join(patched_lines))
 
 
+def install_kanbasu():
+    base_directory = 'static/sass'
+    base_kanbasu_directory = 'vendors/kanbasu'
+    kanbasu_directory = '%s/src/assets/scss' % base_kanbasu_directory
+
+    os.system('git submodule add git@github.com:liip/kanbasu.git %s/%s' % (base_directory, base_kanbasu_directory))
+
+    main_file_path = '%s/kanbasu.scss' % base_directory
+    os.system('cp %s/%s/kanbasu.scss %s' % (base_directory, kanbasu_directory, main_file_path))
+    os.system('echo "@import \'kanbasu\';\n" > static/sass/main.scss')
+    patch_kanbasu(main_file_path, kanbasu_directory)
+
+    settings_directory = '%s/settings' % base_directory
+    os.system('mkdir -p %s' % settings_directory)
+    os.system('cp %s/%s/settings/_settings.scss %s/_kanbasu.scss' % (base_directory, kanbasu_directory, settings_directory))
+
+
+def patch_kanbasu(path, kanbasu_directory):
+    patched_lines = []
+
+    with open(path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if '@import' in line and not 'settings/settings' in line:
+            line = line.replace("@import '", "@import '%s/" % kanbasu_directory)
+
+        patched_lines.append(line)
+
+    with open(path, 'w') as f:
+        f.write(''.join(patched_lines))
+
+
 def pip_compile(path):
     with open('/dev/null', 'wb') as f:
         subprocess.call(['pip-compile', path], stdout=f)
 
 
 if __name__ == '__main__':
-    if '{{ cookiecutter.use_drifter }}' == 'y':
+    use_drifter = '{{ cookiecutter.use_drifter }}' == 'y'
+    use_kanbasu = '{{ cookiecutter.use_kanbasu }}' == 'y'
+
+    if use_drifter:
         install_drifter()
         patch_parameters('virtualization/parameters.yml')
         patch_playbook('virtualization/playbook.yml')
+
+    if use_kanbasu:
+        install_kanbasu()
 
     pip_compile('requirements/dev.in')
     pip_compile('requirements/base.in')
